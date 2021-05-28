4d44a6e7a7aef01b64facbdaa3c210fcf894fc06
Lina Benson <lina.benson@ki.se>
2021-05-28 16:20:10 +0200

    adding outcome for anemia patients testing. changing colours.

--- lib/globals.R
+++ lib/globals.R
@@ -7,10 +7,10 @@ complete <- tidyr::complete
 
 # colours 
 global_kicols <- c(
-  grDevices::rgb(135, 0, 82, maxColorValue = 255),
-  grDevices::rgb(212, 9, 99, maxColorValue = 255),
-  grDevices::rgb(128, 128, 128, maxColorValue = 255),
+  grDevices::rgb(0, 65, 118, maxColorValue = 255),
+  grDevices::rgb(30, 144, 255, maxColorValue = 255),
   grDevices::rgb(151, 216, 218, maxColorValue = 255), # aqua
+  grDevices::rgb(128, 128, 128, maxColorValue = 255), # grey
   grDevices::rgb(136, 196, 197, maxColorValue = 255), # teal
   grDevices::rgb(189, 171, 179, maxColorValue = 255) # lavender
 )
